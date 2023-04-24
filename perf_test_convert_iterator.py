87ea28968665e2408fb21a70754375a3fedc278f
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-04-24 12:20:27 -0400

    Update perf_test_convert_iterator.py

--- perf_test_convert_iterator.py
+++ perf_test_convert_iterator.py
@@ -37,3 +37,90 @@ funcs = [test_convert_by_type_constructor,
          test_convert_by_unpacking, test_convert_by_list_comprehension]
 
 print(*map(get_avg_time_in_ms, funcs))
+
+
+
+from simple_benchmark import BenchmarkBuilder
+from heapq import nsmallest
+from torch import _foreach_abs, sum as torch_sum, cat as torch_cat, stack as torch_stack
+
+b = BenchmarkBuilder()
+
+@b.add_function()
+def l1_reg(model):
+  """
+  This function calculates the l1 norm of the all the tensors in the model
+
+  Args:
+    model: nn.module
+      Neural network instance
+
+  Returns:
+    l1: float
+      L1 norm of the all the tensors in the model
+  """
+  l1 = 0.0
+
+  for param in model.parameters():
+    l1 += torch.sum(torch.abs(param))
+
+  return l1
+
+# @b.add_function()
+# def l1_reg_better_stack(model):
+#   """
+#   This function calculates the l1 norm of the all the tensors in the model
+
+#   Args:
+#     model: nn.module
+#       Neural network instance
+
+#   Returns:
+#     l1: float
+#       L1 norm of the all the tensors in the model
+#   """
+#   return torch_sum(torch_stack(_foreach_abs(list(model.parameters()))))
+
+
+@b.add_function()
+def l1_reg_better_cat(model):
+  """
+  This function calculates the l1 norm of the all the tensors in the model
+
+  Args:
+    model: nn.module
+      Neural network instance
+
+  Returns:
+    l1: float
+      L1 norm of the all the tensors in the model
+  """
+  return torch_sum(torch_cat(tuple(flatten(param_abs) for param_abs in _foreach_abs(list(model.parameters())))))
+  # return torch_sum(torch_cat(_foreach_abs(list(model.parameters()))))
+  # return torch_cat(flatten(param_abs) for param_abs in _foreach_abs(list(model.parameters()))).sum()
+
+
+@b.add_function()
+def l1_reg_better_sum(model):
+  """
+  This function calculates the l1 norm of the all the tensors in the model
+
+  Args:
+    model: nn.module
+      Neural network instance
+
+  Returns:
+    l1: float
+      L1 norm of the all the tensors in the model
+  """
+  return sum(param_abs.sum() for param_abs in _foreach_abs(list(model.parameters())))
+
+@b.add_arguments('L1')
+def argument_provider():
+    for exp in range(2, 22):
+        size = 2**exp
+        yield size, model
+
+
+r = b.run()
+r.plot()
